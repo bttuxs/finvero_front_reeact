86b5ef1cd5dc983de513a968ff9de4d85d3adda5
Baldomero de castilla <bttuxs@gmail.com>
2023-10-24 20:11:54 -0600

    Se integra componente en app inicial

--- src/index.js
+++ src/index.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './index.scss';
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+
+import NotificationContextProvider from './Provider/Notification/NotificactionContextProvider';
+import Loader from './components/Loader/Loader';
+import Alert from './components/Alert/Alert';
+import LoaderStatic from 'src/components/Loader/LoaderStatic';
+
 import reportWebVitals from './reportWebVitals';
+
+
+const AuthComponent = React.lazy(() => import('./Auth/Auth'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <NotificationContextProvider>
+  <Loader />
+  <Alert />
+  <React.Suspense fallback={<LoaderStatic />}>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<AuthComponent />} />
+      </Routes>
+    </BrowserRouter>
+  </React.Suspense>
+</NotificationContextProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
