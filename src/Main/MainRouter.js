e405c1eda4c88c29bfb2b20e6256a999dc6140a9
Baldomero de castilla <bttuxs@gmail.com>
2023-10-25 01:24:27 -0600

    integracion de compoenentes dentro de app base Main

--- src/Main/MainRouter.js
+++ src/Main/MainRouter.js
@@ -1,9 +1,14 @@
 import React from "react";
 import { Route } from "react-router-dom";
 
+const OrdenesComponent = React.lazy(() => import('./Ordenes/Ordene/Ordenes'));
+const CrearOrdenComponent = React.lazy(() => import('./Ordenes/CrearOrden/CrearOrden'));
+
 const MainComponent = React.lazy(() => import('./Main'));
 const RouterMain = (
     <Route path="/main" element={<MainComponent />}>
+        <Route path=""  element={<OrdenesComponent />} />
+        <Route path="crear"  element={<CrearOrdenComponent />} />
     </Route>)
 
 
